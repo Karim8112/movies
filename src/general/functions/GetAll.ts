8ec12e317fa9eb4643251f6415713883ace40e11
Karim8112 <kareem.happal@gmail.com>
2024-12-24 23:32:12 +0300

    sync

--- src/general/functions/GetAll.ts
+++ src/general/functions/GetAll.ts
@@ -1,16 +1,53 @@
+import React, { useEffect } from "react";
 import { IError, state } from "../constants";
+
+export interface RequestInit {
+  method?: string;
+  headers?: HeadersInit;
+  body?: BodyInit | null;
+  mode?: RequestMode;
+  credentials?: RequestCredentials;
+  cache?: RequestCache;
+  redirect?: RequestRedirect;
+  referrer?: string;
+  referrerPolicy?: ReferrerPolicy;
+  integrity?: string;
+  keepalive?: boolean;
+  signal?: AbortSignal | null;
+  window?: any; // Deprecated
+}
+
 export interface IRequestProps<req, res> {
   url: string;
-  data?: req;
-  onState?: (state: state) => void;
-  onError?: (error: IError<res>) => void;
-  onDataBack?: (data: res) => void;
+  // data?: req;
+  requestConfig?: RequestInit;
+  onState?: React.Dispatch<React.SetStateAction<state>>;
+  onError?: React.Dispatch<React.SetStateAction<IError<req>>>;
+  onDataBack?: React.Dispatch<React.SetStateAction<res>>;
+  onFinally?: () => void;
 }
-const GetAll = function <req, res>(props: IRequestProps<req, res>) {
-  const { url, data, onState } = props;
-  if (onState) onState("loading");
-  //   const fetchedData = {
 
-  //   }
+const useGet = function <req, res>(props: IRequestProps<req, res>) {
+  const { url, requestConfig, onState, onError, onDataBack, onFinally } = props;
+
+  if (onState) onState("loading");
+
+  useEffect(() => {
+    fetch(url, requestConfig)
+      .then((res) =>
+        res.json().then((data) => {
+          console.log("this is data", data);
+          if (onDataBack) onDataBack(data);
+          if (onState) onState("done");
+        })
+      )
+      .catch((error) => {
+        if (onState) onState("error");
+        if (onError) onError(error);
+      })
+      .finally(() => {
+        if (onFinally) onFinally();
+      });
+  }, [url, requestConfig]);
 };
-export default GetAll;
+export default useGet;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-// import App from "./App.tsx";
-import ReducerApp from "./ReducerApp.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReducerApp />
+    <App />
   </StrictMode>
 );
